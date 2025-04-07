c102b2db079be0744786b7c1f6534acef560d739
Xperian <110107633+xperian48@users.noreply.github.com>
2025-04-07 06:47:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+var button = document.getElementById("toggle")
+var theme = document.getElementById("theme")
+
 function darktoggle() {
   var element = document.body;
-  element.classList.toggle("dark-mode");
+  element.classList.toggle("dark-mode")
+  theme.classList.toggle("theme-change")
 }
